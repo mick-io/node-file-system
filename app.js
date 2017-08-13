b639b07795660e3060629556be0ecb7508f752b3
mpiereder <mpiereder@gmail.com>
2017-08-13 10:47:01 -0600

    updating repo

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
-const fs = require("fs");
+const fs = require('fs');
+const path = require('path');
 
-function countContent(dirPath, content) {
+const filePath = './bacon-ipsum';
+const foods = {
+    ribs: 0,
+    chicken: 0,
+    jerky: 0,
+    tenderloin: 0,
+    jalapeno: 0,
+    lorem: 0
+};
+
+path.basename('./');
+
+/**
+ *
+ * @param {*} dirPath
+ * @param {*} wordsObj
+ */
+function dirWordCount(dirPath, wordsToCount) {
 
     fs.readdir(dirPath, (error, files) => {
-        if (error) throw new Error(error);
-        for (file of files) {
+        if (error) {
+            throw new Error(error);
+        }
+
+        for (let file of files) {
             let filePath = `${dirPath}/${file}`;
 
-            fs.stat(filePath, (err, stat) => {
-                if (error) throw new Error(error);
+            fs.stat(filePath, (error, stat) => {
+                if (error) {
+                    throw new Error(error);
+                }
                 if (stat.isFile()) {
-                    fs.readFile(filePath, "utf-8", (err, data) => {
-
+                    fs.readFile(filePath, 'utf-8', (error, words) => {
+                        if (error) {
+                            throw new Error(error);
+                        }
+                        words = words.split(' ');
+                        keysInArr(wordsToCount, words);
+                        // console.log(wordsToCount);
                     });
                 }
             });
@@ -19,4 +47,20 @@ function countContent(dirPath, content) {
     });
 }
 
-countContent(`./bacon-ipsum`)
+/**
+ *
+ * @param {*} obj
+ * @param {*} arr
+ */
+function keysInArr(obj, arr) {
+    for (let key in obj) {
+        for (let element of arr) {
+            if (element === key) {
+                obj[key]++;
+            }
+        }
+    }
+    return obj;
+}
+
+dirWordCount(filePath, foods);
